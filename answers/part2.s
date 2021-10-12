ab2b5c0326edc267fc863fa2f7bd1f26573cfa77
a65914zc <zihao.chen-4@student.manchester.ac.uk>
2021-10-12 20:41:54 +0100

    answers

---
+++ answers/part2.s
@@ -0,0 +1,9 @@
+Part 2
+
+Q9) The memory location for "tom" (?) because (?) 
+00000000;Because LDR R0, tom is the initial operation of the ARM, which will be remembered in 00000000.
+
+Q10) If you reset and run the program again without reloading it then what
+happens to the value of "tom" and why: (?)
+It will change constantly, because CPU have to constantly switch routes.
+
